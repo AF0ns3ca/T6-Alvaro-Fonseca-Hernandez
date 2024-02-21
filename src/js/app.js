47ee1d61fde7cb9a7c073da233975cad25cd7e4a
AF0ns3ca <afonsecahdez@gmail.com>
2024-02-21 09:58:38 +0100

    metodo imageFull overlay y estilos footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-   iniciarApp();
+    appInit();
+  });
+  
+  function appInit() {
+    createGallery();
+    let yearSpan = document.getElementById("year");
+    yearSpan.textContent = new Date().getFullYear();
+  }
+  
+  function createGallery() {
+    const galeria = document.querySelector(".galeria-imagenes");
+  
+    for (let i = 1; i <= 15; i++) {
+      const imagen = document.createElement("picture");
+      imagen.innerHTML = `
+              <source srcset="/build/assets/img/thumb/${i}.webp" type="image/webp">
+              <img loading="lazy" width="200" height="300" src="/build/assets/img/thumb/${i}.jpg" alt="imagen galeria">
+          `;
+      imagen.onclick = function () {
+        mostrarImagen(i);
+      };
+  
+      galeria.appendChild(imagen);
+    }
+  }
+  
+  function imageFull(id) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
+          <source srcset="/build/assets/img/big/${id}.webp" type="image/webp">
+          <img loading="lazy" src="/build/assets/img/big/${id}.jpg" alt="imagen galeria">
+      `;
+  
+    // Crea el Overlay con la imagen
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay-img");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+    overlay.onclick = function () {
+      const body = document.querySelector("body");
+      body.classList.remove("fijar-body");
+      overlay.remove();
+    };
+  
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body");
+  }
+  
 
-   function iniciarApp() {
-      crearGaleria();
-   }
-
-   function crearGaleria() {
-      const galeria = document.querySelector(".galeria-imagenes");
-
-      for (let i = 1; i <= 15; i++) {
-         const imagen = document.createElement("picture");
-         galeria.appendChild(imagen);
-         imagen.innerHTML = `
-            
-            <img src="./build/assets/img/thumb/${i}.jpg" width="200" height="300">
-        `;
-      }
-   }
-});
+  
