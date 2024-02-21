e9615dde2c76f7fee14e40a18525ed7d9066aca3
AF0ns3ca <afonsecahdez@gmail.com>
2024-02-21 10:30:04 +0100

    primeras querys

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   
   function appInit() {
+    navFix();
     createGallery();
+    scrollNav();
     
     let yearSpan = document.getElementById("year");
     yearSpan.textContent = new Date().getFullYear();
@@ -47,6 +49,38 @@ document.addEventListener("DOMContentLoaded", function () {
     const body = document.querySelector("body");
     body.appendChild(overlay);
     body.classList.add("fijar-body");
+  }
+
+
+  function scrollNav() {
+    const enlaces = document.querySelectorAll(".nav-list a");
+    enlaces.forEach((enlace) => {
+      enlace.addEventListener("click", function (e) {
+        e.preventDefault();
+        const seccionScroll = e.target.attributes.href.value;
+        const seccion = document.querySelector(seccionScroll);
+        seccion.scrollIntoView({ behavior: "smooth" });
+      });
+    });
+  }
+  
+  function navFix() {
+    const barra = document.querySelector(".main");
+    const video = document.querySelector(".video-hero");
+    const body = document.querySelector("body");
+  
+    window.addEventListener("scroll", function () {
+      if (
+        video.getBoundingClientRect().bottom < 0 &&
+        this.window.innerWidth >= 768
+      ) {
+        barra.classList.add("fijo");
+        body.classList.add("body-scroll");
+      } else {
+        barra.classList.remove("fijo");
+        body.classList.remove("body-scroll");
+      }
+    });
   }
   
 
